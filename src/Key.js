baf3286f1807273f3f6b5916b31970f02e0ebf7d
Sim <zh025700@naver.com>
2022-09-14 20:16:27 +0900

    delte

--- src/Key.js
+++
@@ -1,6 +0,0 @@
-export const KAKAO_JSKEY = `225822da510ac9d45b8e3a0430f091d2`;
-export const CLIENT_ID = `4f9fe8fbf8ab657dcfbf52189e51579c`;
-// export const REDIRECT_URI = `http://localhost:3000/behang/oauth/kakao/callback`;
-// export const REDIRECT_URI = `http://localhost:3000/behang`;
-export const REDIRECT_URI = `https://jaeheon-sim.github.io/behang`;
-export const OPEN_KEY = `vQLxgnWbzLDwZWujom7Tfzceh8%2BaYO4P3WUpTf64AZ5hqBMft3fwVOLksClRPAAft%2BUBmbJOj%2Bw44DPdT3ko0g%3D%3D`;
